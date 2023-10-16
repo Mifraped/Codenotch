4051855a3bf4154bfd32548c4d5fe67f3e85062f
Miguel Franco <francamente@hotmail.es>
2023-10-16 12:49:33 +0200

    reto_1

---
+++ proyectosJavascript(reto2)/proyecto1/index.js
@@ -0,0 +1,16 @@
+function suma(op1, op2){
+    return op1 + op2
+}
+function resta(op1, op2){
+    return op1 - op2
+}
+function multiplica(op1, op2){
+    return op1 * op2
+}
+function divide(op1, op2){
+    return op1 / op2
+}
+function cuadrado(n){
+    return n * n
+}
+module.exports = {suma, resta, multiplica, divide,cuadrado}
---
+++ proyectosJavascript(reto2)/proyecto1/run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index.js")
+
+console.log(myLib.suma(2,3));
+console.log(myLib.resta(2,3));
+console.log(myLib.multiplica(2,3));
+console.log(myLib.divide(2,3));
+console.log(myLib.cuadrado(6));
